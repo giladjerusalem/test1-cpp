22a6105efd3e091b3cf049aac3e8b1347e5e4b9a
giladjerusalem <74672386+giladjerusalem@users.noreply.github.com>
2022-03-09 19:40:31 +0200

    Add files via upload

---
+++ mat.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+#include "mat.hpp"
+
+namespace ariel{
+
+std::string mat (int wide, int length, char symbol1, char symbol2){
+    return "";
+}
+}
---
+++ mat.hpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <iostream>
+
+namespace ariel
+{
+    std::string mat(int wide, int length, char symbol1, char symbol2);
+};
